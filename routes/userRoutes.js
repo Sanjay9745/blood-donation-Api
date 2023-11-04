ea5c125324e7b3d12f89c189ca825819a7f93830
Sanjay9745 <sanjaysanthosh919@gmail.com>
2023-11-04 16:51:57 +0530

    web version added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -59,11 +59,11 @@ router.get("/all", async (req, res) => {
 router.get("/users/:district/:blood_group", async (req, res) => {
     try {
         const {district,blood_group} = req.params;
-        console.log(district,blood_group);
-        const users = await User.find();
+       
+        const users = await User.find({district,blood_group});
         //find users with district and blood group
-        const filteredUsers = users.filter(user => user.district === district && user.blood_group === blood_group);
-        res.status(200).json(filteredUsers);
+        
+        res.status(200).json(users);
 
     } catch (error) {
         res.status(500).json(error);
