f016523a431ccef64ec38321ace1faedd770bb0c
Sanjay9745 <sanjaysanthosh919@gmail.com>
2023-11-04 13:45:30 +0530

    web version added

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 // Initialize Express app
 const app = express();
+const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
+const path = require("path")
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
@@ -13,13 +15,17 @@ mongoose.connect(process.env.MONGODB_URI)
 .catch((error) => {
   console.error('Error connecting to MongoDB:', error);
 });
-
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
+app.use(express.static(path.join(__dirname,'dist')));
 // Define a basic route
 app.use('/api/user', userRoutes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 // Start the server
 const port = 9000;
 app.listen(port, () => {
